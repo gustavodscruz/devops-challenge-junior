8d3e06119b0df3c80ce014f16b9a8e8a2bd8657f
gustavodiasdsc <gustavodiasdsc@gmail.com>
2025-03-15 16:37:01 -0300

    feat: remove old i18n file and add new language support files for localization

--- devops_challenge.php
+++ devops_challenge.php
@@ -7,85 +7,82 @@
 /*
 Plugin Name: Devops challenge Júnior
 Plugin URI: https://apiki.com/
-Description: Sabe de nada, inocente! Ordinária!!
+Description: Sabe de nada, inocente! Ordinária!! Teste Gustavo
 Author: Apiki WordPress
 Version: 1.0
+Text-Domain: devops-challenge
+Domain Path: /languages
 */
  
 if (!defined("ABSPATH")) {
 	exit;
 }
 
+global $global_lyrics;
 
-function apiki_segura_o_tchan($locale)
+function apiki_segura_o_tchan()
 {
 
-	$global_lyrics = [
-		'pt_BR' => "
-			Pau que nasce torto nunca se endireita
-			Menina que requebra a mãe pega na cabeça
-			Pau que nasce torto nunca se endireita
-			Menina que requebra a mãe pega na cabeça
-			Domingo ela não vai (vai, vai)
-			Domingo ela não vai não (vai, vai, vai)
-			Olha, domingo ela não vai (vai, vai)
-			Domingo ela não vai não (vai, vai, vai)
-			O pau que nasce torto nunca se endireita
-			Menina que requebra a mãe pega na cabeça
-			Pau que nasce torto nunca se endireita
-			Menina que requebra a mãe pega na cabeça
-			Segure o tchan
-			Amarre o tchan
-			Segure o tchan tchan tchan tchan
-			Depois de nove meses você vê o resultado
-			Esse é o Gera Samba arrebentando no pedaço
-			Joga ela no meio, mete em cima, mete embaixo
-		",
-		'en_US' => "
-			A crooked tree never straightens
-			A girl who shakes, her mother grabs her head
-			A crooked tree never straightens
-			A girl who shakes, her mother grabs her head
-			She won't go on Sunday (go, go)
-			She won't go on Sunday (go, go, go)
-			Look, she won't go on Sunday (go, go)
-			She won't go on Sunday (go, go)
-			A crooked tree never straightens
-			A girl who shakes, her mother grabs her head
-			A crooked tree never straightens
-			A girl who shakes, her mother grabs her head
-			Hold on to the tchan
-			Love the tchan
-			Hold on to the tchan tchan tchan tchan
-			After nine months you'll see the result
-			This is Gera Samba rocking the place
-			Throw her in the middle, stick her up, stick her down
-		"
-	];
-	
-	$lyrics = $global_lyrics[$locale] ?? $global_lyrics['pt_BR'];
+	$global_lyrics = __("Pau que nasce torto nunca se endireita\n" .
+			"Menina que requebra a mãe pega na cabeça\n" .
+			"Pau que nasce torto nunca se endireita\n" .
+			"Menina que requebra a mãe pega na cabeça\n" .
+			"Domingo ela não vai (vai, vai)\n" .
+			"Domingo ela não vai não (vai, vai, vai)\n" .
+			"Olha, domingo ela não vai (vai, vai)\n" .
+			"Domingo ela não vai não (vai, vai, vai)\n" .
+			"O pau que nasce torto nunca se endireita\n" .
+			"Menina que requebra a mãe pega na cabeça\n" .
+			"Pau que nasce torto nunca se endireita\n" .
+			"Menina que requebra a mãe pega na cabeça\n" .
+			"Segure o tchan\n" .
+			"Amarre o tchan\n" .
+			"Segure o tchan tchan tchan tchan\n" .
+			"Depois de nove meses você vê o resultado\n" .
+			"Esse é o Gera Samba arrebentando no pedaço\n" .
+			"Joga ela no meio, mete em cima, mete embaixo", 'devops-challenge');
+		// 'en_US' => "
+		// 	A crooked tree never straightens
+		// 	A girl who shakes, her mother grabs her head
+		// 	A crooked tree never straightens
+		// 	A girl who shakes, her mother grabs her head
+		// 	She won't go on Sunday (go, go)
+		// 	She won't go on Sunday (go, go, go)
+		// 	Look, she won't go on Sunday (go, go)
+		// 	She won't go on Sunday (go, go)
+		// 	A crooked tree never straightens
+		// 	A girl who shakes, her mother grabs her head
+		// 	A crooked tree never straightens
+		// 	A girl who shakes, her mother grabs her head
+		// 	Hold on to the tchan
+		// 	Love the tchan
+		// 	Hold on to the tchan tchan tchan tchan
+		// 	After nine months you'll see the result
+		// 	This is Gera Samba rocking the place
+		// 	Throw her in the middle, stick her up, stick her down
+		// "
 
-	$lyrics_array = array_filter(array_map('trim', explode("\n", trim($lyrics))));
-	// $lyrics_array = explode("\n", trim($global_lyrics[$locale]));
+	// $lyrics = $global_lyrics[$locale] ?? $global_lyrics['pt_BR'];
 
-	return wptexturize($lyrics_array[array_rand($lyrics_array)]);
-	
+	// $lyrics_array = array_filter(array_map('trim', explode("\n", trim($lyrics))));
+	$lyrics_array = explode("\n", trim($global_lyrics));
+
+	return wptexturize($lyrics_array[mt_rand(0, count( $lyrics_array ) - 1)]);
+
 }
 
 function devops_challenge()
 {
 	$lang   = '';
-	$locale = 'pt_BR';
 	if (substr(get_user_locale(), 0, 3) !== 'pt_') {
 		$lang = 'lang="en"';
-		$locale = 'en_US';
 	}
-	$chosen = apiki_segura_o_tchan($locale);
+	$chosen = apiki_segura_o_tchan();
 
 	printf(
 		'<p id="devop" class="devop" %s> %s</p>',
 		$lang,
-		esc_html__('Segure o Tchan, by Apiki WordPress: ', 'devops_challenge') . esc_html($chosen)
+		esc_html__('Segure o Tchan, by Apiki WordPress: ', 'devops-challenge') . ' ' . esc_html($chosen, 'devops-challenge')
 	);
 }
 
@@ -95,3 +92,9 @@ function devop_css(){
 
 add_action('admin_notices', 'devops_challenge');
 add_action('admin_head', 'devop_css');
+
+add_action('init', 'apiki_load_textdomain');
+
+function apiki_load_textdomain(){
+    load_plugin_textdomain( 'devops-challenge', false, dirname( plugin_basename( __FILE__ ) ) .'/languages');
+}
